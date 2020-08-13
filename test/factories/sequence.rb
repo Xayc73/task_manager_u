f35a1147995b09b8f06c8ddde78ec31e7a4d010e
Sergey Kirillov <xayc73@gmail.com>
2020-08-13 18:14:18 +0400

    Task factory sequences added and all test successfull

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :state] do |n|
     "string#{n}"
+  end
+  sequence :text, aliases: [:description] do |n|
+    "Lorem ipsum lorem ipsum lorem ipsum #{n}"
+  end
+  sequence :expired_at do |n|
+    Date.current.since(n.days)
   end
   sequence :email do |n|
     "person#{n}@example.com"
