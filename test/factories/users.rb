89d2b01d1547ae1e9570f9f21f816451837207bd
Sergey Kirillov <xayc73@gmail.com>
2020-08-13 17:56:21 +0400

    Users factories sequence and successfull test

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -7,4 +7,16 @@ FactoryBot.define do
     avatar
     type { '' }
   end
+
+  factory :developer, parent: :user do
+    type { 'Developer' }
+  end
+
+  factory :manager, parent: :user do
+    type { 'Manager' }
+  end
+
+  factory :admin, parent: :user do
+    type { 'Admin' }
+  end
 end
