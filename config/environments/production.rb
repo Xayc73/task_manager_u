fd3c153e24bf8d98ab6a89da154a5020f3cbd26e
Sergey Kirillov <xayc73@gmail.com>
2020-09-24 13:34:55 +0400

    Host env to prod added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {:host => ENV['MAILER_PROD_HOST']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['MAILER_USERNAME'],
