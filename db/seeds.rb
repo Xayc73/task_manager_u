cf600eaef45a0f2a4af69f342195a8c4b16b46d3
Sergey Kirillov <xayc73@gmail.com>
2020-10-02 00:12:56 +0400

    Seeds for task added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost')
+admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@admin.ru')
 admin.password = 'admin'
 admin.save
 
@@ -8,5 +8,17 @@ admin.save
   u.first_name = "FN#{i}"
   u.last_name = "LN#{i}"
   u.password = "#{i}"
+  u.save
+end
+
+80.times do |i|
+  u = Task.new
+  u.name = "Task ##{i}"
+  u.description = "Description for task ##{i}"
+  u.state = ["new_task", "in_development", "in_qa", "in_code_review", "ready_for_release", "released", "archived"].sample
+  m_offset = rand(Manager.count)
+  a_offset = rand(Developer.count)
+  u.author = Manager.offset(m_offset).first
+  u.assignee = Developer.offset(a_offset).first
   u.save
 end
